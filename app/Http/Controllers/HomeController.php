14138b11b0240c135e9a1de838c1df1c6b9f4bb7
fathitarek <fathitarek208@gmail.com>
2021-04-20 16:15:17 +0200

    report home dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,12 +23,52 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $cloth_amount=0;
+        $food_amount=0;
+        $treatment_amount=0;
+        $payments_cloth = \DB::table('payment')->where('type','cloth')->get();
+        foreach ($payments_cloth as  $cloth) {
+           $cloth_amount+= $cloth->amount;
+        }
+        $payments_food = \DB::table('payment')->where('type','food')->get();
+        foreach ($payments_food as  $food) {
+           $food_amount+= $food->amount;
+        }
+        $payments_treatment = \DB::table('payment')->where('type','treatment')->get();
+        foreach ($payments_treatment as  $treatment) {
+           $treatment_amount+= $treatment->amount;
+        }
+
+// return $cloth_amount;
         $number_of_cases=customers::where('type',0)->count();
         $number_of_sponsors=customers::where('type',1)->count();
+        $highest_donations=\DB::select("SELECT `from_id` ,SUM(amount) as sumtion FROM payment where `type`='donation' GROUP BY `from_id`  order by `sumtion` desc ");  
+        foreach ($highest_donations as $key => $highest_donation) {
+            $highest_donation->name= customers::find($highest_donation->from_id)->name;
+        }  
+        
+        $highest_clothes=\DB::select("SELECT `from_id` ,SUM(amount) as sumtion FROM payment where `type`='cloth' GROUP BY `from_id`  order by `sumtion` desc ");  
+        foreach ($highest_clothes as $key => $highest_cloth) {
+            $highest_cloth->name= customers::find($highest_cloth->from_id)->name;
+        }
+
+        $highest_foods=\DB::select("SELECT `from_id` ,SUM(amount) as sumtion FROM payment where `type`='food' GROUP BY `from_id`  order by `sumtion` desc ");  
+        foreach ($highest_foods as $key => $highest_food) {
+            $highest_food->name= customers::find($highest_food->from_id)->name;
+        }
+
+
+        $highest_treatments=\DB::select("SELECT `from_id` ,SUM(amount) as sumtion FROM payment where `type`='treatment' GROUP BY `from_id`  order by `sumtion` desc ");  
+        foreach ($highest_treatments as $key => $highest_treatment) {
+            $highest_treatment->name= customers::find($highest_treatment->from_id)->name;
+        }
+
+
+// return $highest_donations;
         $highest_wishlists=\DB::select('SELECT `to` ,COUNT(*) as count FROM likes  GROUP BY `to` order by `count` desc');        
             foreach ($highest_wishlists as $key => $highest_wishlist) {
                 $highest_wishlist->customer  = customers::find($highest_wishlist->to);
             }
-        return view('home')->with('highest_wishlists', $highest_wishlists)->with('number_of_cases', $number_of_cases)->with('number_of_sponsors', $number_of_sponsors);
+        return view('home')->with('highest_clothes',$highest_clothes)->with('highest_foods',$highest_foods)->with('highest_treatments',$highest_treatments)->with('highest_donations',$highest_donations)->with('treatment_amount',$treatment_amount)->with('food_amount',$food_amount)->with('cloth_amount',$cloth_amount)->with('highest_wishlists', $highest_wishlists)->with('number_of_cases', $number_of_cases)->with('number_of_sponsors', $number_of_sponsors);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
         <!-- fix for small devices only -->
         <div class="clearfix visible-sm-block"></div>
 
-        <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="col-md-2 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-green"><i class="ion ion-ios-people-outline"></i></span>
 
@@ -29,7 +29,7 @@
           <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="col-md-2 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
 
@@ -47,7 +47,68 @@
           <!-- /.info-box -->
         </div>
         <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="ion-tshirt"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Cloth Bank</span>
+              <span class="info-box-number">
+                @if(isset($cloth_amount))
+                  {{$cloth_amount}} $
+                  @else
+                  0 $
+              @endif
+            </span>
+            
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <div class="col-md-2 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="ion-android-restaurant"></i></span>
+  
+            <div class="info-box-content">
+              <span class="info-box-text">Food Bank</span>
+              <span class="info-box-number">
+                @if(isset($food_amount))
+                  {{$food_amount}} $
+                  @else
+                  0 $
+              @endif
+            </span>
+            
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="ion-android-bar"></i></span>
+  
+            <div class="info-box-content">
+              <span class="info-box-text">Treatment Bank</span>
+              <span class="info-box-number">
+                @if(isset($treatment_amount))
+                  {{$treatment_amount}} $
+                  @else
+                  0 $
+              @endif
+            </span>
+            
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div> 
       </div>
+     
+
+      
+    
       <!-- /.row -->
 
       <!-- /.row -->
@@ -122,7 +183,7 @@
           <!-- TABLE: LATEST ORDERS -->
            <div class="box box-info">
             <div class="box-header with-border">
-              <h3 class="box-title"> Highest wishlists products</h3>
+              <h3 class="box-title"> Highest Donations For  Cases ( {{count ($highest_donations)}} )</h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -142,11 +203,140 @@
                   </tr>
                   </thead>
                   <tbody>
-                    @if(isset($highest_wishlists))
-                    @foreach($highest_wishlists as $highest_wishlist)
+                    @if(isset($highest_donations))
+                    @foreach($highest_donations as $highest_donation)
                   <tr>
-                    <td>{!! $highest_wishlist->count !!}</td>
-                    {{-- <td>{!! $highest_wishlist->product !!}</td> --}}
+                    <td>{!! $highest_donation->name !!}</td>
+                    <td>{!! $highest_donation->sumtion !!} $</td>
+                    
+                  </tr>
+               @endforeach
+               @endif
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.table-responsive -->
+            </div>
+            <!-- /.box-body -->
+           
+            <!-- /.box-footer -->
+          </div>
+
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title"> Highest Donations For Food Bank ( {{count ($highest_foods)}} )</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div> 
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="table-responsive">
+                <table class="table no-margin">
+                  <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>$</th>
+                    
+                  </tr>
+                  </thead>
+                  <tbody>
+                    @if(isset($highest_foods))
+                    @foreach($highest_foods as $highest_food)
+                  <tr>
+                    <td>{!! $highest_food->name !!}</td>
+                    <td>{!! $highest_food->sumtion !!} $</td>
+                    
+                  </tr>
+               @endforeach
+               @endif
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.table-responsive -->
+            </div>
+            <!-- /.box-body -->
+           
+            <!-- /.box-footer -->
+          </div>
+
+
+
+
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title"> Highest Donations For Clothes Bank ( {{count ($highest_clothes)}} )</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div> 
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="table-responsive">
+                <table class="table no-margin">
+                  <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>$</th>
+                    
+                  </tr>
+                  </thead>
+                  <tbody>
+                    @if(isset($highest_clothes))
+                    @foreach($highest_clothes as $highest_clothe)
+                  <tr>
+                    <td>{!! $highest_clothe->name !!}</td>
+                    <td>{!! $highest_clothe->sumtion !!} $</td>
+                    
+                  </tr>
+               @endforeach
+               @endif
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.table-responsive -->
+            </div>
+            <!-- /.box-body -->
+           
+            <!-- /.box-footer -->
+          </div>
+
+
+
+
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title"> Highest Donations For Treatment Bank ( {{count ($highest_treatments)}} )</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div> 
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="table-responsive">
+                <table class="table no-margin">
+                  <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>$</th>
+                    
+                  </tr>
+                  </thead>
+                  <tbody>
+                    @if(isset($highest_treatments))
+                    @foreach($highest_treatments as $highest_treatment)
+                  <tr>
+                    <td>{!! $highest_treatment->name !!}</td>
+                    <td>{!! $highest_treatment->sumtion !!} $</td>
                     
                   </tr>
                @endforeach
